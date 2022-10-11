024f0186a800442c95e6eb9cb3386b5aa1be0417
SC <sunweichao@rayclear.com>
2022-10-11 16:57:42 +0800

    fix bug

--- Pods/SuperPlayer/Demo/TXLiteAVDemo/SuperPlayerKit/SuperPlayer/SuperPlayerView.m
+++ Pods/SuperPlayer/Demo/TXLiteAVDemo/SuperPlayerKit/SuperPlayer/SuperPlayerView.m
@@ -2222,8 +2222,16 @@ TXLiveBaseDelegate,TXLivePlayListener,TXVodPlayListener>
     NSString *       videoURL   = self.playerModel.playingDefinitionUrl;
     NSURLComponents *components = [NSURLComponents componentsWithString:videoURL];
     NSString *       scheme     = [[components scheme] lowercaseString];
-    if ([scheme isEqualToString:@"rtmp"]) {
-        playType = PLAY_TYPE_LIVE_RTMP;
+    if ([scheme isEqualToString:@"rtmp"])
+    {
+        if(self.playerModel.ACC)
+        {
+            playType = PLAY_TYPE_LIVE_RTMP_ACC;
+        }
+        else
+        {
+            playType = PLAY_TYPE_LIVE_RTMP;
+        }
     } else if ([scheme hasPrefix:@"http"] && [[components path].lowercaseString hasSuffix:@".flv"]) {
         playType = PLAY_TYPE_LIVE_FLV;
     }
